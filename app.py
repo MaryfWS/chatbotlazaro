18568546910a16fcfebdbe8e994c99fb386ec7c9
MaryfWS <a20203468@pucp.edu.pe>
2025-10-02 23:45:14 +0000

    s

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ tanto por su precio como por sus beneficios clínicos, sociales y emocionales.
 # ------------------------
 # INTERFAZ
 # ------------------------
-st.title("🤖♿ Chatbot Lázaro")
+st.title("🤖🧑‍🦽 Chatbot Lázaro")
 st.write("""Hola! Soy Patri del equipo Lázaro. Dime ¿en qué puedo ayudarte hoy?""")
 
 # Historial de chat
